9a48300729cde067c8d707bf5e2bffe4ce4188e3
tarun khanna <tarun.kh@gmail.com>
2016-11-10 17:34:58 +0100

    Updated code_data cleaning complete

--- data_cleaning/Eurostat_data.R
+++ data_cleaning/Eurostat_data.R
@@ -1,67 +1,11 @@
-library(rio)
-library(plyr)
-library(dplyr)
-library(rvest)
-library(xml2)
-library(countrycode)
-
-#Independent variables
-
-#5. Energy technologies patent applications to the EPO by priority year
-
-energy_patent <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/patents_eu.csv')
-
-#select the necessary columns
-head(energy_patent)
-energy_patent <- select(energy_patent, GEO, TIME, Value)
-head(energy_patent)
-
-#treat value column as numeric + clean missing values
-energy_patent[,3] <- as.numeric(energy_patent[,3])
-summary(energy_patent$Value)
-#energy_patent <- subset(x = energy_patent,!is.na(Value))
-#summary(energy_patent)
-
-#arranging data - ascending
-#head(energy_patent)
-#energy_patent<- arrange(energy_patent, GEO, TIME)
-#head(energy_patent)
-
-#create new column for country codes + adjusting order of columns
-#energy_patent["iso2c"] <- ""
-#energy_patent <- select(energy_patent, iso2c, GEO, TIME, Value)
-#energy_patent <- rename(energy_patent, year = TIME, country = GEO)
-#head(energy_patent)
-
-#6. Long term interest rates (10 year bond yields) 
-
-interest_rates <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/interest_rates_annual.csv')
-
-#select the necessary columns
-head(interest_rates)
-interest_rates <- select(interest_rates, GEO, TIME, Value)
-head(energy_patent)
-
-#treat value column as numeric + clean missing values
-interest_rates[,3] <- as.numeric(interest_rates[,3])
-summary(interest_rates$Value)
-#interest_rates <- subset(x = interest_rates,!is.na(Value))
-#summary(interest_rates)
-
-#7. Oil price
-
-oil_price <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/crude_opec_all.csv')
-
-#treat value column as numeric + clean missing values
-oil_price[,2] <- as.numeric(oil_price[,2])
-summary(oil_price $Value)
-
-#8. Installed Renewable Energy Capacity
+### DEPENDENT VARIABLES
+ 
+### Installed Renewable Energy Capacity
 
 solar <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_1_Data.csv')
-wind1 <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_1_Data.csv')
-wind2 <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_1_Data.csv')
-wind3 <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_1_Data.csv')
+wind1 <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_2_Data.csv')
+wind2 <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_3_Data.csv')
+wind3 <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/nrg_105a_4_Data.csv')
 
 #select the necessary columns
 solar <- select(solar, GEO, TIME, Value)
@@ -75,14 +19,69 @@ wind1[,'Value'] <- as.numeric(wind1[,'Value'])
 wind2[,'Value'] <- as.numeric(wind2[,'Value'])
 wind3[,'Value'] <- as.numeric(wind3[,'Value'])
 
+#assigning NA = zero generation
+solar[is.na(solar)] <- 0
+wind1[is.na(wind1)] <- 0
+wind2[is.na(wind2)] <- 0
+wind3[is.na(wind3)] <- 0
+
 #creating dependent variables
-re_generation <- merge(wind1, wind2, by = c('GEO', 'TIME'))
-re_generation <- merge(re_generation, wind3 , by = c('GEO', 'TIME'))
+re_generation <- merge(wind1, wind2, by = c('GEO', 'TIME'), all.x = T)
+re_generation <- merge(re_generation, wind3 , by = c('GEO', 'TIME'), all.x = T)
 re_generation$wind_gen <- re_generation$Value.x+ re_generation$Value.y+ re_generation$Value
-re_generation <- select(re_generation, GEO, TIME, wind)
-re_generation <- merge(re_generation, solar, by = c('GEO', 'TIME'))
+re_generation <- select(re_generation, GEO, TIME, wind_gen)
+re_generation <- merge(re_generation, solar, by = c('GEO', 'TIME'), all.x = T)
 names(re_generation)[4] <- "solar_gen" 
 re_generation$re_gen <- re_generation$wind_gen + re_generation$solar_gen
+ 
+### INDEPENDENT VARIABLES
 
+### Energy technologies patent applications to the EPO by priority year
 
+energy_patent <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/patents_eu.csv')
+
+#select the necessary columns
+head(energy_patent)
+energy_patent <- select(energy_patent, GEO, TIME, Value)
+head(energy_patent)
+
+#treat value column as numeric + clean missing values
+energy_patent[,3] <- as.numeric(energy_patent[,3])
+names(energy_patent)[3] <- "patents"
+summary(energy_patent$Value)
+
+### Long term interest rates (10 year bond yields) 
+
+interest_rates <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/interest_rates_annual.csv')
+
+#select the necessary columns
+head(interest_rates)
+interest_rates <- select(interest_rates, GEO, TIME, Value)
+
+#treat value column as numeric + clean missing values
+interest_rates[,3] <- as.numeric(interest_rates[,3])
+summary(interest_rates$Value)
+names(interest_rates)[3] <- "interest_rate"
+#interest_rates <- subset(x = interest_rates,!is.na(Value))
+#summary(interest_rates)
+
+### Oil price (from OPEC)
+
+oil_price <- import('https://raw.githubusercontent.com/Camila-RV/VieiraKhanna_Assignment3/master/data_raw/crude_opec_all.csv')
+
+#treat value column as numeric + clean missing values
+oil_price[,2] <- as.numeric(oil_price[,2])
+summary(oil_price $Value)
+names(oil_price)[1] <- "TIME"
+names(oil_price)[2] <- "oil_price"
+names(oil_price)[3] <- "GEO"
+head(oil_price)
+
+### Merging the Eurostat databases
+Combine_EuroStat <- merge(re_generation, energy_patent, by =c("GEO","TIME"))
+Combine_EuroStat <- merge(Combine_EuroStat, interest_rates, by =c("GEO","TIME"))
+Combine_EuroStat <- merge(Combine_EuroStat, oil_price, by =c("GEO","TIME"))
+names(Combine_EuroStat)[1] <- "country" 
+names(Combine_EuroStat)[2] <- "year"
+rm(list = c("wind1", "wind2", "wind3","solar","energy_patent","interest_rates","oil_price","re_generation"))
 
